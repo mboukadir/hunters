18aa9f3f2df80b63a70c50c224620036487e69c2
Mohammed BOUKADIR <mohammed.boukadir@pumpkin-app.com>
2019-06-12 10:27:48 +0200

    Update circle ci config

--- app/src/main/java/com/mb/hunters/ui/common/HuntersNavigator.kt
+++ app/src/main/java/com/mb/hunters/ui/common/HuntersNavigator.kt
@@ -20,7 +20,6 @@ import android.app.Activity
 import android.graphics.BitmapFactory
 import android.net.Uri
 import android.support.customtabs.CustomTabsIntent
-import android.support.v4.app.FragmentActivity
 import com.mb.hunters.R
 import com.mb.hunters.ui.base.Navigator
 import com.mb.hunters.ui.common.chromtab.CustomTabActivityHelper
